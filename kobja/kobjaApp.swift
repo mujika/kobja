170b27b9c09c96982558f67f6efae6bb5f5e0f9c
mujika <mujika4042@gmail.com>
2025-09-21 19:11:24 +0900

    Initial Commit

---
+++ kobja/kobjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kobjaApp.swift
+//  kobja
+//
+//  Created by 新村彰啓 on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct kobjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kobjaTests/kobjaTests.swift
@@ -0,0 +1,17 @@
+//
+//  kobjaTests.swift
+//  kobjaTests
+//
+//  Created by 新村彰啓 on 9/21/25.
+//
+
+import Testing
+@testable import kobja
+
+struct kobjaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
